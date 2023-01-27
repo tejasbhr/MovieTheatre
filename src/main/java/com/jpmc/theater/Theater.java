f79eff4738dbf64dc7997ab8622f109a974d45fb
tejas <tejasbhr@gmail.com>
2023-01-27 17:10:05 -0500

    remove unwanted code

--- src/main/java/com/jpmc/theater/Theater.java
+++ src/main/java/com/jpmc/theater/Theater.java
@@ -9,21 +9,6 @@ import java.time.LocalTime;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import java.lang.reflect.Type;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParseException;
-import com.google.gson.JsonPrimitive;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
 
 public class Theater {
 
